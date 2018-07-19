4396b207ab9c6887690b223854ade682088c6f6a
Dalton Duncan <daltonduncan44@gmail.com>
2018-07-18 21:06:00 -0400

    reset add + word array

---
+++ game.js
@@ -0,0 +1,8 @@
+// create a array of words
+    const words = ["thor", "ironman", "spiderman", "antman", "thanos", "blackwidow", "starlord", "groot", "loki", "hawkeye", "hulk", "ultron", "gamora", "vision", "rocket", "drax", "blackpanther"]
+// choose word randomly
+//create underscores based on length of word 
+//get users guess
+//chec if guess is right 
+//if right push to right array
+//if wrong push to wrong array
