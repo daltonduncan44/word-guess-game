74a80c1096d786e221ae324b613884db071c867a
Dalton Duncan <daltonduncan44@gmail.com>
2018-07-19 08:16:47 -0400

    randomizer

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,9 @@
 // create a array of words
-    const words = ["thor", "ironman", "spiderman", "antman", "thanos", "blackwidow", "starlord", "groot", "loki", "hawkeye", "hulk", "ultron", "gamora", "vision", "rocket", "drax", "blackpanther"]
+    const word = ["thor", "ironman", "spiderman", "antman", "thanos", "blackwidow", "starlord", "groot", "loki", "hawkeye", "hulk", "ultron", "gamora", "vision", "rocket", "drax", "blackpanther"]
 // choose word randomly
+    var randNum = Math.floor(Math.random() * word.length);
+    var choosenWord = word[randNum];
+    console.log(choosenWord);
 //create underscores based on length of word 
 //get users guess
 //chec if guess is right 
