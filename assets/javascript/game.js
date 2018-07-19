0d0f082f10e76299d5dece6249017cdd8e2fed4d
Dalton Duncan <daltonduncan44@gmail.com>
2018-07-19 17:48:39 -0400

    progree on underlines

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,26 +10,39 @@ var underScore = [];
 var rightWord = [];
 //var to create an array for the wrougn choices
 var wrongWord = [];
+ ///dom manip
+var docUnderScore = document.getElementsByClassName("underScore");
+
 //create underscores based on length of word 
 var genUnderscore = () => {
-    for (var i = 0; i < choosenWord.length; i++) {
+ for (var i = 0; i < choosenWord.length; i++) {
         underScore.push("_");
+        docUnderScore[0].innerHtml = underScore.join(" ");
     }
     return underScore;
 }
 console.log(genUnderscore());
 //get users guess
 document.addEventListener("keypress", (event) => {
-    var keyword = String.fromCharCode(event.keycode);
-    console.log(keyword);
+    let keyword = String.fromCharCode(event.keycode);
+    if (choosenWord.indexOf(keyword) > -1) {
+        // if else statement to move inputs to the right array
+        rightWord.push(keyword);
+        //re place underscores with right letter
+        underScore[choosenWord.indexOf(keyword)] = keyword;
+        //checks to see if word matched the guesses
+        if (underScore.join("") == choosenWord) {
+            //alerts the winner
+            alert("You are a Winner")
+        }
+    }
+    else {
+        wrongWord.push(keyword);
+    }
+
 });
 //chec if guess is right 
-// if else statement to move inputs to the right array
-if (keyword.indexOf(choosenWord) > -1) {
 
-    rightWord.push(keyword);
- } else{
-    wrongWord.push(keyword);
-}
+
 //if right push to right array
 //if wrong push to wrong array
